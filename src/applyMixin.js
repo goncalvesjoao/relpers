42a3e0662ba2bd78461c47fe9cddfe17b80c7723
Sergio Daniel Xalambrí <daniel@platzi.com>
2016-01-06 15:16:22 -0500

    don't use Object.assign

--- src/applyMixin.js
+++ src/applyMixin.js
@@ -1,37 +1,35 @@
+function applyMixin(target, mixin) {
+  // apply mixin to the target class
+  // and return the mixed class
+  return Object.keys(mixin).reduce((targetClass, key) => {
+    // if the target class has the prop or method
+    if (targetClass[key] || targetClass.prototype[key]) {
+      // return it untouched
+      return targetClass;
+    }
+
+    // get the value
+    const value = mixin[key];
+
+    // if the value is a functoin
+    if (typeof value === 'function') {
+      // add the value to the prototype
+      // of the targetClass
+      targetClass.prototype[key] = value;
+      // return the target class
+      return targetClass;
+    }
+
+    // add the value as a static prop
+    targetClass[key] = value;
+
+    // return the class
+    return targetClass;
+  }, target);
+}
+
 function applyMixinBuilder(...mixins) {
-  return (target) => {
-    // merge array of mixins into one single mixin
-    const mixin = Object.assign({}, ...mixins.reverse());
-
-    // apply mixin to the target class
-    // and return the mixed class
-    return Object.keys(mixin)
-      .reduce((targetClass, key) => {
-        // if the target class has the prop or method
-        if (targetClass[key] || targetClass.prototype[key]) {
-          // return it untouched
-          return targetClass;
-        }
-
-        // get the value
-        const value = mixin[key];
-
-        // if the value is a functoin
-        if (typeof value === 'function') {
-          // add the value to the prototype
-          // of the targetClass
-          targetClass.prototype[key] = value;
-          // return the target class
-          return targetClass;
-        }
-
-        // add the value as a static prop
-        targetClass[key] = value;
-
-        // return the class
-        return targetClass;
-      }, target);
-  };
+  return target => mixins.reduce(applyMixin, target);
 }
 
 export default applyMixinBuilder;
