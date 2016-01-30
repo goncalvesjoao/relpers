4afcaa4ca39008a6a5b36fab85f8966054532560
goncalvesjoao <goncalves.joao@gmail.com>
2016-01-30 01:22:29 +0000

    version and dist bump

--- dist/standalone/bundle/relpers.js
+++ dist/standalone/bundle/relpers.js
@@ -91,15 +91,34 @@ webpackJsonp([0],[
 /* 3 */
 /***/ function(module, exports) {
 
+	/*
+	 * Apply mixin (POJO) to ES6 class
+	 * @param target {Class} - The class to decorate
+	 * @param mixin {Object} - The object mixin to apply
+	 * @return {Class} - Decorated class
+	 */
 	'use strict';
 
 	Object.defineProperty(exports, '__esModule', {
 	  value: true
 	});
+
+	var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
 	function applyMixin(target, mixin) {
 	  // apply mixin to the target class
 	  // and return the mixed class
 	  return Object.keys(mixin).reduce(function (targetClass, key) {
+	    // if the key is contextTypes
+	    if (key === 'contextTypes') {
+	      // merge the target and mixin contextTypes
+	      var _value = _extends({}, mixin[key], targetClass[key]);
+	      // apply it
+	      targetClass[key] = _value;
+	      // return the target class
+	      return targetClass;
+	    }
+
 	    // if the target class has the prop or method
 	    if (targetClass[key] || targetClass.prototype[key]) {
 	      // return it untouched
@@ -126,6 +145,11 @@ webpackJsonp([0],[
 	  }, target);
 	}
 
+	/*
+	 * Apply one or more mixins (POJOs) to a ES6 class
+	 * @param mixins {Array} - List of mixins to apply
+	 * @return {Function} - Function to apply the list of mixins
+	 */
 	function applyMixinBuilder() {
 	  for (var _len = arguments.length, mixins = Array(_len), _key = 0; _key < _len; _key++) {
 	    mixins[_key] = arguments[_key];
